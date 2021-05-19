1394805d09c975032bba4575a1a035b64ba7e784
sunzepeng <389343209@qq.com>
2021-05-19 17:28:22 +0800

    test_one

--- src/main/java/com/example/demo/Service/MessageServiceImpl.java
+++ src/main/java/com/example/demo/Service/MessageServiceImpl.java
@@ -1,19 +1,27 @@
 package com.example.demo.Service;
 
+import com.example.demo.Dao.AbnormalDao;
+import com.example.demo.Dao.AbnormalRepository;
 import com.example.demo.Dao.MessageDao;
 import com.example.demo.Dao.MessageRepository;
 import com.example.demo.Model.Message;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class MessageServiceImpl implements MessageService{
     @Resource
     private MessageRepository messageRepository;
+    private AbnormalRepository abnormalRepository;
 
     @Override
-    public String MessageReport(Message message){
+    public String MessageReport(Message message) throws IllegalAccessException {
 
 
         MessageDao messageDao = new MessageDao();
@@ -28,8 +36,30 @@ public class MessageServiceImpl implements MessageService{
 
         messageRepository.save(messageDao);
 
+        if(message.getHealthcondition() == 1 || message.getRisklocation() == 1 || message.getRiskcontact() == 1)
+        {
+            AbnormalDao abnormalDao = new AbnormalDao();
 
-
+            Class clazz = abnormalDao.getClass();
+            Field[] fields = clazz.getDeclaredFields();
+            Class clazzm = message.getClass();
+            Field[] fieldm = clazzm.getDeclaredFields();
+            for(Field a:fields){
+                for(Field m:fieldm){
+                    if(a.getName().equals(m.getName())){
+                        a.setAccessible(true);
+                        m.setAccessible(true);
+                        a.set(abnormalDao,m.get(message));
+                    }
+                }
+            }
+            abnormalRepository.save(abnormalDao);
+//            abnormalDao.setOpenid(message.getOpenid());
+//            abnormalDao.setHealthcondition(message.getHealthcondition());
+//            abnormalDao.setTemperature(message.getTemperature());
+//            abnormalDao.setSymptom(message.getSymptom());
+//            abnormalDao
+        }
 
 
 
